bb0a0ffc57c075073cfadc94e55f52b6a8f84fab
George Zisis <georgezisis@gmail.com>
2025-05-07 14:36:51 +0300

    Save form data to local storage and load from them so page always loads all data

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const myLibrary = []
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false, 1)
-addBookToLibrary("The Hunger Games", "Suzanne Collins", 374, true, 2)
-addBookToLibrary("Divergent", "Veronica Roth", 487, true, 3)
-addBookToLibrary("The Maze Runner", "James Dashner", 384, false, 4)
+defaultBook1 = createBook("The Hobbit", "J.R.R Tolkien", 295, false, 1)
+defaultBook2 = createBook("The Hunger Games", "Suzanne Collins", 374, true, 2)
+defaultBook3 = createBook("Divergent", "Veronica Roth", 487, true, 3)
+defaultBook4 = createBook("The Maze Runner", "James Dashner", 384, false, 4)
+
+myLibrary.push(defaultBook1)
+myLibrary.push(defaultBook2)
+myLibrary.push(defaultBook3)
+myLibrary.push(defaultBook4)
 
 containerDiv = document.querySelector(".container")
 
@@ -11,21 +16,18 @@ for (const book of myLibrary) {
     createBookCard(book)
 }
 
-document.querySelector("form").addEventListener("submit", getFormValues)
-
-function getFormValues(event) {
-    event.preventDefault()
-    const title = event.target.title.value
-    const author = event.target.author.value
-    const pages = event.target.pages.value
-    const read = true
-    const id = myLibrary.length + 1
-    addBookToLibrary(title, author, pages, read, id)
+for (i = 0; i < localStorage.length; i++) {
+    let newBook = JSON.parse(localStorage.getItem(`book${i + 5}`))
+    if (newBook === null) {
+        continue
+    }
+    console.log(i + 5)
+    console.log(localStorage.length)
+    myLibrary.push(newBook)
     createBookCard(myLibrary[myLibrary.length - 1])
 }
 
-
-console.log(myLibrary.length)
+document.querySelector("form").addEventListener("submit", addBookToLocalStorage)
 
 function Book(title, author, numPages, hasRead, id) {
     this.title = title
@@ -40,9 +42,8 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.numPages} pages, ${readPrintStatement}, id:${this.id}`
 }
 
-function addBookToLibrary(title, author, numPages, hasRead, id) {
-    const newBook = new Book(title, author, numPages, hasRead, id)
-    myLibrary.push(newBook)
+function createBook(title, author, numPages, hasRead, id) {
+    return new Book(title, author, numPages, hasRead, id)
 }
 
 function createBookCard(book) {
@@ -66,4 +67,33 @@ function createBookCard(book) {
     theCard.appendChild(theHasRead)
 
     containerDiv.appendChild(theCard)
+}
+
+function getFormValues(event) {
+    event.preventDefault()
+    const title = event.target.title.value
+    const author = event.target.author.value
+    const pages = event.target.pages.value
+    const read = true
+    const id = myLibrary.length + 1
+    return [title, author, pages, read, id]
+}
+
+function addBookToLocalStorage(event) {
+    [title, author, pages, read, id] = getFormValues(event)
+    let newBook = createBook(title, author, pages, read, id)
+    localStorage.setItem(`book${id}`, JSON.stringify(newBook))
+    retrieveBookFromLocalStorage(id)
+}
+
+function retrieveBookFromLocalStorage(id) {
+    // for (i = 0; i < localStorage.length; i++) {
+    //     let newBook = JSON.parse(localStorage.getItem(`book${i + 5}`))
+    //     myLibrary.push(newBook)
+    //     createBookCard(myLibrary[myLibrary.length - 1])
+    // }
+
+    let newBook = JSON.parse(localStorage.getItem(`book${id}`))
+    myLibrary.push(newBook)
+    createBookCard(myLibrary[myLibrary.length - 1])
 }
